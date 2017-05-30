847bb7f4d62d912efa1d519f837ec373a1482707
Geng <snggeng@gmail.com>
2017-05-30 10:32:04 +0800

    updated reducer.js for reducer cases set_entries, next, vote

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,3 +1,13 @@
-export default function reducer(state, action) {
-  // Figure out which function to call and call it
+import {setEntries, next, vote} from './core'
+
+export default function reducer (state, action) {
+  switch (action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries)
+    case 'NEXT':
+      return next(state)
+    case 'VOTE':
+      return vote(state, action.entry)
+  }
+  return state
 }
