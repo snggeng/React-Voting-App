1eb7c1996d3e904567ac288776f4566d1e9c3f84
Geng <snggeng@gmail.com>
2017-05-30 10:23:03 +0800

    added new next(state) function which merges state on next to core.js

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -3,3 +3,11 @@ import {List} from 'immutable'
 export function setEntries (state, entries) {
   return state.set('entries', List(entries))
 }
+
+export function next (state) {
+  const entries = state.get('entries')
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  })
+}
