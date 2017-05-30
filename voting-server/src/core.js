1113d6813db03c1f47b991095acb1f564e2012ec
Geng <snggeng@gmail.com>
2017-05-30 10:45:40 +0800

    updated core.js and reducer.js to reflect test

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -22,8 +22,8 @@ export function next (state) {
 }
 
 export function vote (state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   )
