021afcaeb06fbf358b619b867ceb060551abd87d
Geng <snggeng@gmail.com>
2017-06-02 14:55:52 +0800

    problem with test

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {
   renderIntoDocument,
   scryRenderedDOMComponentsWithTag
 } from 'react-addons-test-utils';
+
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
